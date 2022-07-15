8119edcfd055a2678f51da7a3f6943ba47208812
Danii <danielabarrera@hotmail.com.ar>
2022-07-15 16:23:44 -0300

    Proyectos y Footer

--- Front-End/src/app/app.module.ts
+++ Front-End/src/app/app.module.ts
@@ -10,6 +10,7 @@ import { EducacionComponent } from './components/educacion/educacion.component';
 import { SkillsComponent } from './components/skills/skills.component';
 import { ProyectosComponent } from './components/proyectos/proyectos.component';
 import { NgCircleProgressModule } from 'ng-circle-progress';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
@@ -20,6 +21,7 @@ import { NgCircleProgressModule } from 'ng-circle-progress';
     EducacionComponent,
     SkillsComponent,
     ProyectosComponent,
+    FooterComponent,
   ],
   imports: [
     BrowserModule,
