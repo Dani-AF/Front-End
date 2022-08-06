afa1e2696ad57949eae2d5a10fda4c0f32fe1de3
Danii <danielabarrera@hotmail.com.ar>
2022-08-06 02:34:15 -0300

    home

--- Front-End/src/app/app.module.ts
+++ Front-End/src/app/app.module.ts
@@ -10,7 +10,9 @@ import { SkillsComponent } from './components/skills/skills.component';
 import { ProyectosComponent } from './components/proyectos/proyectos.component';
 import { NgCircleProgressModule } from 'ng-circle-progress';
 import { FooterComponent } from './components/footer/footer.component';
-import { HttpClient, HttpClientModule } from '@angular/common/http'
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component'
 
 @NgModule({
   declarations: [
@@ -22,6 +24,8 @@ import { HttpClient, HttpClientModule } from '@angular/common/http'
     SkillsComponent,
     ProyectosComponent,
     FooterComponent,
+    HomeComponent,
+    LoginComponent,
   ],
   imports: [
     BrowserModule,
